0944bc7e9fa317e002536c3a4e34108fd844d6ca
cunyizju <cunyicom@outlook.com>
2024-08-16 11:29:33 +0200

    test CDPM.sh

---
+++ testCDPM.sh
@@ -0,0 +1,19 @@
+cd ./tests/sm
+
+../../build/oofem -f con1dpm1.in > con1dpm1.out &
+../../build/oofem -f con1dpm2.in > con1dpm2.out &
+../../build/oofem -f con1dpm3.in > con1dpm3.out &
+../../build/oofem -f con2dpm1.in > con2dpm1.out &
+../../build/oofem -f con2dpm2.in > con2dpm2.out &
+../../build/oofem -f con2dpm3.in > con2dpm3.out &
+../../build/oofem -f con2dpm4.in > con2dpm4.out &
+../../build/oofem -f con2dpm5.in > con2dpm5.out &
+../../build/oofem -f con2dpm6.in > con2dpm6.out &
+../../build/oofem -f con2dpm7.in > con2dpm7.out &
+../../build/oofem -f con2dpm8.in > con2dpm8.out &
+../../build/oofem -f con2dpm9.in > con2dpm9.out &
+../../build/oofem -f con2dpm10.in > con2dpm10.out &
+../../build/oofem -f con2dpm11.in > con2dpm11.out &
+../../build/oofem -f con2dpm12.in > con2dpm12.out &
+
+cd ../..
