d34555e2cb88dcf55c182c7dc7a3df2e207ce63f
cunyizju <cunyicom@outlook.com>
2024-08-23 09:44:30 +0200

    nonlinear static solver

--- src/sm/nlinearStaticSolver/nlinearstatic.C
+++ src/sm/nlinearStaticSolver/nlinearstatic.C
@@ -32,7 +32,7 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "sm/EngineeringModels/nlinearstatic.h"
+#include "sm/nlinearStaticSolver/nlinearstatic.h"
 #include "sm/Elements/structuralelement.h"
 #include "nummet.h"
 #include "timestep.h"
