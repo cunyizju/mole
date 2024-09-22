c07d1aa27c10631b50df44395046a893409c8d31
cunyizju <cunyicom@outlook.com>
2024-09-22 17:46:16 +0200

    license

---
+++ header.cpp
@@ -0,0 +1,37 @@
+/*
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2024   Borek Patzak
+ *
+ *                      Modified by Cunyi Li in 2024
+ *
+ *                   GNU LESSER GENERAL PUBLIC LICENSE
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ */
+
+ /*
+ *            ###    ###   #####   ##       #######
+ *           ##  ### ##  ##   ##  ##       ##
+ *          ##   #  ##  ##   ##  ##       #######
+ *         ##      ##  ##   ##  ##       ##
+ *        ##      ##  ##   ##  ##       ##
+ *       ##      ##   #####   #######  #######
+ *
+ * Mole is based on OOFEM, which has been developed since 1993 by 
+ * Borek Patzak and his colleagues at Czech Technical University.
+ *
+ * Mole focuses on solid mechanics, especially concrete models.
+ * Modified by Cunyi Li
+ *
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ */
